6c0316904eec8dd774d83fd2818f2f20a4da5b77
Manjinderrana <manjindersingh3890@gmail.com>
2024-11-25 11:56:18 +0530

    fix: mail

--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,18 +1,8 @@
 import nodemailer from 'nodemailer'
 import { ApiError } from './error'
-// import path from "path"
-// import fs from "fs"
-// import ejs from "ejs"
 
 export const sendMail = async (email: string, subject: string, text: string, html: string) => {
   try {
-    // if (!Maildata?.email) return
-    // const templatePath = path.resolve("Templates", Maildata.HTMLtemplate)
-
-    // const template = fs.readFileSync(templatePath, { encoding: "utf8" })
-
-    // const html = ejs.render(template, Maildata)
-
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       secure: true,
